9191e0c6764f6f079fafdc1a0af9e8b7950390d0
KratiGupta <kratigupta8545@gmail.com>
2021-08-11 21:42:25 +0530

    Added palindrome checker successfully

---
+++ Deque.java
@@ -0,0 +1,23 @@
+package dsAlgorithm;
+
+public class Deque<E> {
+	
+
+		public E data;
+		public Deque<E>next;
+		public Deque<E>pre;
+		
+		public Deque()
+		{
+			this.next=null;
+			this.pre=null;
+		}
+		
+		public Deque(E val)
+		{
+			this.data=val;
+			this.next=null;
+			this.pre=null;
+		}
+
+}
