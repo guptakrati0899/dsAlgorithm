70da5750c48bbd17225465ce46aa02cdc9967c15
KratiGupta <kratigupta8545@gmail.com>
2021-08-12 14:47:21 +0530

    Successfully added Prime Number using 2d Array program

---
+++ PrimeNumber2D.java
@@ -0,0 +1,63 @@
+package dsAlgorithm;
+
+public class PrimeNumber2D {
+	
+	public static void main(String[] args) {
+		int start = 0;
+		int end = 100;
+
+		int[][] primeNumber = new int[10][];
+
+		int j1 = 0, j2 = 0;
+		// declaration of array is done
+		for (int i = 0; i < 10; i++) {
+			for (int j = start; j <= end; j++) {
+				if (isPrime(j)) {
+					j1++;
+				}
+			}
+			primeNumber[i] = new int[j1];
+			start = end + 1;
+			end += 100;
+			j1 = 0;
+
+		}
+		start = 0;
+		end = 100;
+		// putting values in array
+		for (int i = 0; i < 10; i++) {
+			for (int j = start; j <= end; j++) {
+
+				if (isPrime(j)) {
+					primeNumber[i][j2] = j;
+					j2++;
+				}
+
+			}
+			start = end + 1;
+			end += 100;
+			j2 = 0;
+		}
+
+		// printing array
+		for (int i = 0; i < primeNumber.length; i++) {
+			for (int j = 0; j < primeNumber[i].length; j++)
+				System.out.print(primeNumber[i][j] + " ");
+			System.out.println();
+		}
+	}
+	
+	public static boolean isPrime(int num) {
+		if (num == 0 || num == 1) {
+			return false;
+		}
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+
